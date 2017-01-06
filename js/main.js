c14b179757692702d4e9c6b81e0d698871e3a4d5
Edward Gao <egao@sent.com>
2017-01-05 21:11:49 -1000

    added everything and does not work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 (( Phaser, Game, CFG ) => {
 
-  const preload = _ => {
+  Game.hero = null;
+  Game.platformsGroup = null;
+  Game.cursors = null;
 
+  const preload = _ => {
+    game.load.atlasJSONHash(
+      CFG.ASSETS.GFX,
+      CFG.ASSETS.ATLAS_PNG_PATH,
+      CFG.ASSETS.ATLAS_JSON_PATH
+    );
   };
 
   const create = _ => {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    game.physics.arcade.gravity.y = CFG.GRAVITY;
+    game.stage.backgroundColor = CFG.BG_COLOR;
 
+    Game.cursors = game.input.keyboard.createCursorKeys();
+
+    Game.platformsGroup = game.add.group();
+    Game.LevelDesigner.load(game, 1);
+
+    Game.hero = new Game.Hero(game, 500, CFG.GAME_HEIGHT - 200);
+    game.camera.follow(Game.hero.sprite, null, CFG.CAMERA_LERP, CFG.CAMERA_LERP);
   };
 
   const update = _ => {
